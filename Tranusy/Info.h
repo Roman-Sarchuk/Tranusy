c9c0436eb64a81a748bbc7c916972cf1f6436c3b
Roman-Sarchuk <91730539+Roman-Sarchuk@users.noreply.github.com>
2024-06-27 18:03:48 +0300

    🔼 Improve the info text

--- Tranusy/Info.h
+++ Tranusy/Info.h
@@ -76,21 +76,21 @@ namespace Tranusy {
 			});
 			this->menuStrip1->Location = System::Drawing::Point(0, 0);
 			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Size = System::Drawing::Size(478, 28);
+			this->menuStrip1->Size = System::Drawing::Size(686, 28);
 			this->menuStrip1->TabIndex = 1;
 			this->menuStrip1->Text = L"menuStrip1";
 			// 
 			// englishToolStripMenuItem
 			// 
 			this->englishToolStripMenuItem->Name = L"englishToolStripMenuItem";
-			this->englishToolStripMenuItem->Size = System::Drawing::Size(70, 26);
+			this->englishToolStripMenuItem->Size = System::Drawing::Size(70, 24);
 			this->englishToolStripMenuItem->Text = L"English";
 			this->englishToolStripMenuItem->Click += gcnew System::EventHandler(this, &Info::englishToolStripMenuItem_Click);
 			// 
 			// ukraineToolStripMenuItem
 			// 
 			this->ukraineToolStripMenuItem->Name = L"ukraineToolStripMenuItem";
-			this->ukraineToolStripMenuItem->Size = System::Drawing::Size(73, 26);
+			this->ukraineToolStripMenuItem->Size = System::Drawing::Size(73, 24);
 			this->ukraineToolStripMenuItem->Text = L"Ukraine";
 			this->ukraineToolStripMenuItem->Click += gcnew System::EventHandler(this, &Info::ukraineToolStripMenuItem_Click);
 			// 
@@ -102,7 +102,7 @@ namespace Tranusy {
 			this->appInfo->Location = System::Drawing::Point(13, 32);
 			this->appInfo->Name = L"appInfo";
 			this->appInfo->ReadOnly = true;
-			this->appInfo->Size = System::Drawing::Size(453, 488);
+			this->appInfo->Size = System::Drawing::Size(661, 488);
 			this->appInfo->TabIndex = 2;
 			this->appInfo->Text = L"";
 			// 
@@ -110,7 +110,7 @@ namespace Tranusy {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(478, 532);
+			this->ClientSize = System::Drawing::Size(686, 532);
 			this->Controls->Add(this->appInfo);
 			this->Controls->Add(this->menuStrip1);
 			this->MainMenuStrip = this->menuStrip1;
@@ -154,6 +154,13 @@ namespace Tranusy {
 		• Simple conversion from the one system to another.
 		• Option to show conversion steps for better understanding.
 		• Multiple converting.
+		How to use 'multiple mode':
+		Input as this pattern: number [from] [to]
+		You will get the result as: result [to]
+		Example of input:
+		101101 [2] [10]
+		-14201 [5] [24]
+		AB04D.5F [16] [3]
 		Warning:
 		Tranusy's using base type of data as integer {From −9 223 372 036 854 775 808 to 9 223 372 036 854 775 807} and doubel {1.7E +/- 308 (15 characters)}.
 		Thus Tranusy can't convert the very huge numbers yet.
@@ -168,6 +175,13 @@ namespace Tranusy {
 		addSimpleText("• Simple conversion from the one system to another.");
 		addSimpleText("• Option to show conversion steps for better understanding.");
 		addSimpleText("• Multiple converting.\n");
+		addTitleText("How to use 'multiple mode':");
+		addSimpleText("Input as this pattern: number [from] [to]");
+		addSimpleText("You will get the result as: result [to]");
+		addSimpleText("Example of input:");
+		addSimpleText("101101 [2] [10]");
+		addSimpleText("-14201 [5] [24]");
+		addSimpleText("AB04D.5F [16] [3]\n");
 		addTitleText("Warning:");
 		addSimpleText("Tranusy's using base type of data as integer {From −9 223 372 036 854 775 808 to 9 223 372 036 854 775 807} and doubel {1.7E +/- 308 (15 characters)}.");
 		addSimpleText("Thus Tranusy can't convert the very huge numbers yet.");
@@ -183,6 +197,13 @@ namespace Tranusy {
 		• Просте перетворення з однієї системи в іншу.
 		• Можливість показати кроки перетворення для кращого розуміння.
 		• Множинне перетворення.
+		Як використовувати 'multiple mode':
+		Введіть як нацьому шаблоні: число [з] [у]
+		Ти отримаєш результат у вигляді: результат [у]
+		Приклад введення:
+		101101 [2] [10]
+		-14201 [5] [24]
+		AB04D.5F [16] [3]
 		Увага:
 		Tranusy використовує базовий тип даних як int {Від −9 223 372 036 854 775 808 до 9 223 372 036 854 775 807} і double {1,7E +/- 308 (15 символів)}.
 		Таким чином Tranusy ще не може конвертувати дуже великі числа.
@@ -197,6 +218,13 @@ namespace Tranusy {
 		addSimpleText("• Просте перетворення з однієї системи в іншу.");
 		addSimpleText("• Можливість показати кроки перетворення для кращого розуміння.");
 		addSimpleText("• Множинне перетворення.\n");
+		addTitleText("Як використовувати 'multiple mode':");
+		addSimpleText("Введіть як нацьому шаблоні: число [з] [у]");
+		addSimpleText("Ти отримаєш результат у вигляді: результат [у]");
+		addSimpleText("Приклад введення:");
+		addSimpleText("101101 [2] [10]");
+		addSimpleText("-14201 [5] [24]");
+		addSimpleText("AB04D.5F [16] [3]\n");
 		addTitleText("Увага:");
 		addSimpleText("Tranusy використовує базовий тип даних як int {Від −9 223 372 036 854 775 808 до 9 223 372 036 854 775 807} і double {1,7E +/- 308 (15 символів)}.");
 		addSimpleText("Таким чином Tranusy ще не може конвертувати дуже великі числа.");
